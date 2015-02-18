96fded77339883dabdca63b8bf71f7715af3609e
giria <joan@giria.com>
2015-02-18 17:35:09 +0100

    Initial Commit

---
+++ CustomDynamicAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomDynamicAlertView
+//
+//  Created by Joan Barrull Ribalta on 18/02/15.
+//  Copyright (c) 2015 com.giria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
